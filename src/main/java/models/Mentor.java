86aadf72b95c87ca8ae4dc1fc2754a0b89c36df6
Joanna Gora <joanna.gora@protonmail.com>
2018-08-15 20:09:02 +0100

    created setup of the classes

--- src/main/java/models/Mentor.java
+++ src/main/java/models/Mentor.java
@@ -1,4 +1,27 @@
 package models;
 
 public class Mentor {
+
+    private int id;
+    private String name;
+
+    public Mentor(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -1,4 +1,47 @@
 package models;
 
 public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private int enrolmentNumber;
+
+    public Student(String name, int age, int enrolmentNumber) {
+        this.name = name;
+        this.age = age;
+        this.enrolmentNumber = enrolmentNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getEnrolmentNumber() {
+        return enrolmentNumber;
+    }
+
+    public void setEnrolmentNumber(int enrolmentNumber) {
+        this.enrolmentNumber = enrolmentNumber;
+    }
 }
